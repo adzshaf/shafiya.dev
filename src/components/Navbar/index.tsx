c43cb116e149e45c6887271cef9d4058b53bc68f
Shafiya Adzhani <adz.arsym@gmail.com>
2020-10-08 18:32:15 +0800

    hide other navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,17 @@
 import { FC } from "react";
 import styles from "./Navbar.module.css";
+import Link from "next/link";
 
 const Navbar: FC = () => {
   return (
     <div className="flex justify-between py-8 absolute w-full">
-      <h1 className={`${styles.title}`}>shafiya.dev</h1>
+      <Link href="/">
+        <h1 className={`${styles.title}`}>shafiya.dev</h1>
+      </Link>
       <div className="flex justify-center items-center self-end">
-        <h1 className={`${styles.nav}`}>home</h1>
-        <h1 className={`${styles.nav} ml-16`}>writing</h1>
-        <h1 className={`${styles.nav} ml-16`}>work</h1>
-        <h1 className={`${styles.nav} ml-16`}>me</h1>
+        <Link href="/">
+          <h1 className={`${styles.nav}`}>home</h1>
+        </Link>
       </div>
     </div>
   );
